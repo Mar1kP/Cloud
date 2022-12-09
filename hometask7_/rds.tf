1f93e8fc9923db51485a5d66797b6b7cb1dc08a4
Mar1kP <91725998+Mar1kP@users.noreply.github.com>
2022-12-09 07:31:55 +0200

    Add files via upload

---
+++ hometask7_/rds.tf
@@ -0,0 +1,24 @@
+resource "aws_db_instance" "Lab7" {
+    identifier = "mysqldb"
+    storage_type = "gp2"
+    allocated_storage = 20
+
+    engine = "mysql"
+    engine_version = "5.7"
+    instance_class = "db.t2.micro"
+
+    db_name = "dbtest"
+    username = var.username
+    password = var.password
+    
+    parameter_group_name = "default.mysql5.7"
+    port = "3306"
+    vpc_security_group_ids = [aws_security_group.lab7_sg.id]
+    db_subnet_group_name = aws_db_subnet_group.db_sg.id
+    skip_final_snapshot = true
+    publicly_accessible = true
+    
+    tags = {
+      Name = "Lab 7"
+    }
+}
