9ee82a83ea0cd1400e600b0e7f4561720d17999c
Mar1kP <91725998+Mar1kP@users.noreply.github.com>
2023-01-11 14:16:22 +0200

    update ht3

--- hometask3/run_custom_ami.sh
+++ hometask3/run_custom_ami.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-IMAGE_ID=$(aws ec2 describe-images --owners 141848694151 --query 'Images[*].ImageId' --output text) \
-&& echo $IMAGE_ID \
-&& aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type t2.micro --key-name MyKeyPairl2 --security-group-ids $SG --user-data file://user_data.sh
+IMAGE_ID=$(aws ec2 describe-images --owners 141848694151 --query 'Images[*].ImageId' --output text)
+echo $IMAGE_ID
+aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type t2.micro --key-name MyKeyPair1 --security-group-ids sg-010f765f16f442e61 --user-data file://user_data.sh
