8f38fd2502fa432de4ab3c856ae7615b8a317619
Mar1kP <91725998+Mar1kP@users.noreply.github.com>
2023-01-15 21:47:54 +0200

    update 3

--- hometask3_/run_custom_ami.sh
+++ hometask3_/run_custom_ami.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-
-# Retrieve IMAGE_ID
 IMAGE_ID=`aws ec2 describe-images --owners 141848694151 --query Images[0].ImageId --output text`
-
-# Retrieve SECURITY_GROUP_ID
 GROUP_ID=`aws ec2 describe-security-groups --query SecurityGroups[1].GroupId --output text`
 
-# Run Instance
 INSTANCE_ID=`aws ec2 run-instances \
     --image-id $IMAGE_ID \
     --count 1 \
@@ -16,10 +11,5 @@ INSTANCE_ID=`aws ec2 run-instances \
     --user-data file://user_script.sh \
     --query Instances[0].InstanceId --output text`
 
-# Retrieve IP address
-IP_ADDRESS=`aws ec2 describe-instances \
-    --instance-ids $INSTANCE_ID \
-    --query Reservations[0].Instances[0].PublicDnsName --output text`
+IP_ADDRESS=`aws ec2 describe-instances --instance-ids $INSTANCE_ID --query Reservations[0].Instances[0].PublicDnsName --output text`
 echo $IP_ADDRESS
-
-sleep 15
