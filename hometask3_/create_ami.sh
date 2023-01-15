fc6786b999643bc3e4a56c6607958f7bfa8c8c0a
Mar1kP <91725998+Mar1kP@users.noreply.github.com>
2023-01-15 21:48:53 +0200

    update 3

--- hometask3_/create_ami.sh
+++ hometask3_/create_ami.sh
@@ -1,14 +1,7 @@
 #!/bin/bash 
 
-# Retrieve INSTANCE_ID 
-INSTANCE_ID=`aws ec2 describe-instances \
-    --filters Name=instance-state-name,Values=stopped \
+INSTANCE_ID=`aws ec2 describe-instances --filters Name=instance-state-name,Values=stopped \
     --query Reservations[0].Instances[0].InstanceId --output text`
 
-# Create an image
-aws ec2 create-image \
-    --instance-id $INSTANCE_ID \
-    --name "Test Image" \
+aws ec2 create-image --instance-id $INSTANCE_ID --name "Test Image" \
     --description "An Image of my instance"
-
-sleep 15
