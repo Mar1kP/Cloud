aa4d337f7605c7a4f28360878a349b4dbcb9edde
Mar1kP <91725998+Mar1kP@users.noreply.github.com>
2023-01-15 21:56:07 +0200

    update 5

--- hometask5/script.sh
+++ hometask5/script.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-
-# Create SNS topic
 TOPIC_ARN=`aws sns create-topic --name health_checking --query 'TopicArn' --output text`
+SUB_ARN=`aws sns subscribe --topic-arn $TOPIC_ARN --protocol email --notification-endpoint pigruhm@gmail.com --query 'SubscriptionArn' --output text`
 
-# Subscribe SNS topic
-SUB_ARN=`aws sns subscribe \
-    --topic-arn $TOPIC_ARN \
-    --protocol email \
-    --notification-endpoint pigruhm@gmail.com \
-    --query 'SubscriptionArn' --output text`
-
-# Set up a cloudwatch
 aws cloudwatch put-metric-alarm \
     --alarm-name health_checking \
     --alarm-description "Alarm instance is deleted!!!" \
