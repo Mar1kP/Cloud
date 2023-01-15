52a7f4e05e0c322e81ba727bf7b8cef53269e070
Mar1kP <91725998+Mar1kP@users.noreply.github.com>
2023-01-15 21:51:14 +0200

    update 3

--- hometask3_/stop_instance.sh
+++ hometask3_/stop_instance.sh
@@ -1,7 +1,4 @@
-
 #!/bin/bash
 
-INSTANCE_ID=`aws ec2 describe-instances \
-    --filters Name=instance-state-name,Values=running \
-    --query Reservations[0].Instances[0].InstanceId --output text`
+INSTANCE_ID=`aws ec2 describe-instances --filters Name=instance-state-name,Values=running --query Reservations[0].Instances[0].InstanceId --output text`
 aws ec2 stop-instances --instance-ids $INSTANCE_ID
