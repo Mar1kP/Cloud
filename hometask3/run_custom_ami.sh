b837507f7cb7c5c73cc0d7c064980c238321530b
Mar1kP <91725998+Mar1kP@users.noreply.github.com>
2023-01-15 21:35:12 +0200

    delete 3

--- hometask3/run_custom_ami.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-IMAGE_ID=$(aws ec2 describe-images --owners 141848694151 --query 'Images[*].ImageId' --output text)
-echo $IMAGE_ID
-aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type t2.micro --key-name MyKeyPair1 --security-group-ids sg-010f765f16f442e61 --user-data file://user_data.sh
--- hometask3/user_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install httpd -y
-systemctl start httpd
-systemctl enable httpd
-touch /var/www/html/index.html
-echo "<h1> At $(hostname -f) </h1>" > /var/www/html/index.html
