1fdd92e7fe978e1a311f2ff9988b893613bc3418
Mar1kP <91725998+Mar1kP@users.noreply.github.com>
2023-01-16 20:56:27 +0200

    update 8

--- hometask8-/scripts.sh
+++ hometask8-/scripts.sh
@@ -3,13 +3,13 @@
 aws iam create-role --role-name lambda-ex --assume-role-policy-document file://trust-policy.json
 aws iam attach-role-policy --role-name lambda-ex --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
 zip function.zip index.py
-aws lambda create-function --function-name my-function \
-    --zip-file fileb://function.zip --handler index.handler --runtime python3.9 \
+aws lambda create-function --function-name lab8pfunc \
+    --zip-file fileb://function.zip --handler index.handler --runtime python3.7 \
     --role arn:aws:iam::141848694151:role/lambda-ex
-aws lambda invoke --function-name my-function out --log-type Tail \
+aws lambda invoke --function-name lab8pfunc out --log-type Tail \
     --query 'LogResult' --output text |  base64 -d
 aws lambda list-functions --max-items 10
-aws lambda get-function --function-name my-function
+aws lambda get-function --function-name lab8pfunc
 
 
 
